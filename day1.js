df16cda2a036a7f49e73cc755554b75194ac48d5
Daniel de Oliveira <devdoliveiradd@gmail.com>
2022-11-28 23:01:00 -0300

    Exercício Day 1

--- day1.js
+++ day1.js
@@ -1,1 +1,14 @@
-/* Olá, fico feliz que você venho até aqui e verificou o meu repositório! Obrigado, espero que goste do meu código! */
+// 1 – Calcule a média de diversas notas do usuário.
+
+const fisica = 6.3;
+const matematica = 9.0;
+const quimica = 7.5;
+const portugues = 6.3
+
+const media = (fisica + matematica + quimica + portugues) / 2;
+
+if(media >= 7) {
+    console.log(`A média foi: ${media}. Parabéns, você foi aprovado!`);
+} else {
+    console.log(`A média foi: ${media}. Infelizmente você foi reprovado!`);
+}
