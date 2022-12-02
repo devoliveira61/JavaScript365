4ea0a2c6d4cdd5e9905eba7b72f3be6eb6c14694
Daniel de Oliveira <devdoliveiradd@gmail.com>
2022-12-01 23:54:16 -0300

    Exercício Day 3

---
+++ day3.js
@@ -0,0 +1,15 @@
+/* Criar uma calculadora avançada */
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+const operador = "/";
+
+if (operador === "+") {
+  console.log(primeiroNumero + segundoNumero);
+} else if (operador === "*") {
+  console.log(primeiroNumero * segundoNumero);
+} else if (operador === "/") {
+  console.log(primeiroNumero / segundoNumero);
+} else if (operador === "-") {
+  console.log(primeiroNumero - segundoNumero);
+}
