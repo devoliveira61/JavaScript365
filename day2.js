e38e620e6ded63abaad234174d7d0e4f838c785d
Daniel de Oliveira <devdoliveiradd@gmail.com>
2022-12-13 15:51:23 -0300

    Restart

--- day2.js
+++
@@ -1,8 +0,0 @@
-/* Ontem tive que realizar um trabalho que deveria ser feito no dia (prioridade total) e acabei terminando tarde e perdendo um dia, mas vamos lá! */
-// Crie uma calculadora de IMC
-
-const alturaUsuario = 1.75;
-const pesoUsuario = 56.7;
-const calculandoIMC = pesoUsuario / (alturaUsuario * alturaUsuario);
-
-console.log(`O seu IMC é: ${calculandoIMC.toFixed(2)}`)
